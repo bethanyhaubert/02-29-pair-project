b0638d0042f91b461482f77e0e257008f8e8cc7b
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-29 16:18:30 -0600

    user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,14 @@
+MyApp.get "users/new" do
+end
+
+MyApp.post "users/create" do
+end
+
+MyApp.get "users/:id/view" do
+end
+
+MyApp.post "users/:id/edit" do
+end
+
+MyApp.post "users/:id/delete" do
+end
