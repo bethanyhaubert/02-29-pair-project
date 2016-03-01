d3fc4b3e0da8c9a81827c8bbc291d50134f41305
Beth Haubert <bethany.haubert@gmail.com>
2016-03-01 11:20:44 -0600

    added movie crud

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -10,10 +10,25 @@ MyApp.get "/movies/search/results" do
 end
 
 MyApp.get "/movies/new" do
+	erb :'movies/new'
 end
 
 MyApp.post "/movies/create" do
+	@movie = Movie.new
+	@movie.title = params[:title]
+	@movie.director = params[:director]
+	@movie.image = params[:image]
+	@movie.critic_rating = params[:rating]
+
+	if @movie.is_valid == true
+	 	@movie.save
+		redirect "/movies/#{@movie.id}/view"
+	else
+		@error_object = @movie
+		erb :"error"
+	end
 end
 
 MyApp.get "/movies/:id/view" do
+	erb :"movies/view"
 end
