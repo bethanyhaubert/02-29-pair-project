c68a03d207aa4fc1b57a52fca86cc1838166b092
Beth Haubert <bethany.haubert@gmail.com>
2016-03-01 10:02:01 -0600

    login controller

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,22 @@
 MyApp.post "/login" do
+	@user = User.find_by_email(params["email"])
+
+	  if params["password"] != nil
+	    if @user.password == params["password"]
+	      session["user_id"] = @user.id
+	      session["temporary_error_message"] = nil
+	      redirect "/"
+	    else
+	      session["temporary_error_message"] = "Incorrect login information"
+	     redirect "/"
+	    end
+	  else
+	      session["temporary_error_message"] = "Please create a new account"
+	     redirect "/"
+	  end
 end
 
 MyApp.post "/logout" do
+ 	session["user_id"] = nil
+  redirect "/"
 end
