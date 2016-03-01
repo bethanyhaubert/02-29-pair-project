a84702bf9634088f1be5de1ec83e8e0b9a79f0b1
Beth Haubert <bethany.haubert@gmail.com>
2016-03-01 14:56:23 -0600

    adding inside result form controllers

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -3,7 +3,16 @@ MyApp.get "/results/list" do
   erb :"results/gallery"
 end
 
-MyApp.post "/results/:id/new" do
+MyApp.post "/results/:id/add" do
+	@current_movie = Movie.find_by_id(params[:id])
+	@result = Result.new
+	@result.q1 = params[:q1]
+	@result.q2 = params[:q2]
+	@result.q3 = params[:q3]
+	@result.movie_id = @current_movie.id
+	@result.user_id = @current_user.id
+	@result.save
+  redirect "/movies/#{@movie.id}/view"
 end
 
 MyApp.get "/results/:id/edit" do
