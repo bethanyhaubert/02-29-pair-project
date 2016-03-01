ce2e62c7279c66b0345c3e51e391bcec7d5f1169
Beth Haubert <bethany.haubert@gmail.com>
2016-03-01 10:50:35 -0600

    home page

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,17 +1,19 @@
-MyApp.get "movies/home" do
+MyApp.get "/" do
+	@random_movie = "http://techno2013.ucoz.com/dhoom2-18-1.jpg"
+	erb :"movies/home"
 end
 
-MyApp.post "movies/search" do
+MyApp.post "/movies/search" do
 end
 
-MyApp.get "movies/search/results" do
+MyApp.get "/movies/search/results" do
 end
 
-MyApp.get "movies/new" do
+MyApp.get "/movies/new" do
 end
 
-MyApp.post "movies/create" do
+MyApp.post "/movies/create" do
 end
 
-MyApp.get "movies/:id/view" do
+MyApp.get "/movies/:id/view" do
 end
